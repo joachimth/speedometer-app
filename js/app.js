cad374bb737ca277b6f660c6a1372e8efcb75e5b
joachimth <46874512+joachimth@users.noreply.github.com>
2023-12-02 22:32:43 +0000

    Deploying to gh-pages from @ joachimth/speedometer-app@b90979f51eb9b1bdc42d8c0d0c7be99bcd9e55e5 🚀

--- js/app.js
+++ js/app.js
@@ -2,10 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const speedDisplay = document.getElementById('speed-display');
 
     function updateSpeed(position) {
-        const speedInKmh = (position.coords.speed * 3.6).toFixed(2);
-        speedDisplay.textContent = speedInKmh;
+        const speedInKmh = Math.round(position.coords.speed * 3.6);
+    document.getElementById('speed-display').textContent = speedInKmh;
+    // Opdater hastighedsgrænsen her, når du har data
     }
 
+// Antag at du har en funktion til at hente hastighedsgrænsen
+function updateSpeedLimit(speedLimit) {
+    document.getElementById('speed-limit').textContent = speedLimit;
+}
+    
     function handleError(error) {
         console.warn('ERROR(' + error.code + '): ' + error.message);
     }
