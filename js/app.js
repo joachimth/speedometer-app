1df244c66b9afd04ddb6cf94ce38649b7ca2b367
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2023-12-04 16:16:18 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const speedDisplay = document.getElementById('speed-display');
 
     function updateSpeed(position) {
         const speedInKmh = Math.round(position.coords.speed * 3.6);
-    document.getElementById('speed-display').textContent = speedInKmh;
-    // Opdater hastighedsgrænsen her, når du har data
+        document.getElementById('speed-display').textContent = speedInKmh;
+        getSpeedLimit(position.coords.latitude, position.coords.longitude);
     }
 
-// Antag at du har en funktion til at hente hastighedsgrænsen
-function updateSpeedLimit(speedLimit) {
-    document.getElementById('speed-limit').textContent = speedLimit;
-}
-    
+    function updateSpeedLimit(speedLimit) {
+        document.getElementById('speed-limit').textContent = speedLimit;
+    }
+
     function handleError(error) {
         console.warn('ERROR(' + error.code + '): ' + error.message);
     }
@@ -24,20 +25,24 @@ function updateSpeedLimit(speedLimit) {
     } else {
         alert("Geolocation is not supported by this browser.");
     }
-    
-function getSpeedLimit(lat, lng) {
-    const apiKey = 'AIzaSyBiNnRq_bOUJ2i1gvmPz1Rzmp42eYd3-lg';
-    const url = `https://roads.googleapis.com/v1/speedLimits?path=${lat},${lng}&key=${apiKey}`;
 
-    axios.get(url)
-        .then(response => {
-            const speedLimit = response.data.speedLimits[0].speedLimit;
-            // Opdater din UI med hastighedsgrænsen
-        })
-        .catch(error => {
-            console.error("Error fetching speed limit:", error);
-        });
-}
+    function getSpeedLimit(lat, lng) {
+        const apiKey = 'AIzaSyBiNnRq_bOUJ2i1gvmPz1Rzmp42eYd3-lg';
+        const url = `https://roads.googleapis.com/v1/speedLimits?path=${lat},${lng}&key=${apiKey}`;
+
+        axios.get(url)
+            .then(response => {
+                if (response.data.speedLimits && response.data.speedLimits.length > 0) {
+                    const speedLimit = response.data.speedLimits[0].speedLimit;
+                    updateSpeedLimit(speedLimit);
+                } else {
+                    console.log("No speed limit data available");
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching speed limit:", error);
+            });
+    }
 
     // Keep the screen awake
     // Note: This functionality might need additional implementation based on the browser.
