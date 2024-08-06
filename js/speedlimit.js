4865ae5afc382cb1e0c85cb5b5d87ff04ae81edc
joachimth <46874512+joachimth@users.noreply.github.com>
2024-08-06 19:40:13 +0000

    Deploying to gh-pages from @ joachimth/speedometer-app@41ed214507d1d24c3ac09d13047fc5970b419045 🚀

---
+++ js/speedlimit.js
@@ -0,0 +1,27 @@
+import { updateRoadHistory, isConsistentRoadData } from './helpers.js';
+
+export function getSpeedLimit(coordinates, roadHistory, speedLimitDisplay, roadInfoDisplay) {
+    const query = coordinates.map(coord => `way(around:30,${coord.lat},${coord.lng})["highway"];`).join('');
+    const overpassUrl = `https://overpass-api.de/api/interpreter?data=[out:json];(${query});out body;`;
+
+    axios.get(overpassUrl)
+        .then(response => {
+            const roads = response.data.elements;
+            if (roads.length > 0) {
+                const nearestRoad = roads[0];
+                const speedLimit = nearestRoad.tags.maxspeed || "Ukendt";
+                const roadName = nearestRoad.tags.name || nearestRoad.tags.ref || "Ukendt";
+
+                updateRoadHistory(roadHistory, { roadName, speedLimit }, 3);
+                if (isConsistentRoadData(roadHistory)) {
+                    speedLimitDisplay.textContent = speedLimit;
+                    roadInfoDisplay.textContent = `Vej: ${roadName}`;
+                }
+            } else {
+                console.log("Ingen veje fundet i nærheden");
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching road data from OSM:", error);
+        });
+}
