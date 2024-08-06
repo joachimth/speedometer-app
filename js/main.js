99b4d3dc9883bc85f018f8dd131e96d688a7fcb7
joachimth <46874512+joachimth@users.noreply.github.com>
2024-08-06 19:47:46 +0000

    Deploying to gh-pages from @ joachimth/speedometer-app@9130d2bc6a4ecc534659205230f4d1d9778ec6c2 🚀

--- js/main.js
+++ js/main.js
@@ -19,13 +19,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const ROAD_CHECK_THRESHOLD = 3; // Antal af ens vejopslag
     let roadHistory = []; // Holder de seneste vejopslag
     let $watchHandler_showPosition = null;
-    let $watchHandler_showLimit = null;
 
     button.addEventListener("click", manualStart);
 
     function manualStart() {
         if ($watchHandler_showPosition === null) {
-            initLocation(showPosition, errorCallback);
+            $watchHandler_showPosition = initLocation(showPosition, errorCallback);
             document.getElementById("icon-start").style.display = "none";
             document.getElementById("icon-ok").style.display = "inline-block";
         }
@@ -34,6 +33,15 @@ document.addEventListener("DOMContentLoaded", function() {
     function showPosition(position) {
         const speedInKmh = Math.round(position.coords.speed * 3.6);
         speedDisplay.textContent = isNaN(speedInKmh) ? '-' : speedInKmh;
+
+        if (collectedCoordinates.length > 0) {
+            const lastPosition = collectedCoordinates[collectedCoordinates.length - 1];
+            const direction = calculateDirection(
+                lastPosition.lat, lastPosition.lng,
+                position.coords.latitude, position.coords.longitude
+            );
+            console.log(`Kører i retning: ${direction.toFixed(2)}°`);
+        }
 
         collectedCoordinates.push({ lat: position.coords.latitude, lng: position.coords.longitude });
 
